ec00b34e8312fe8b225f70677b368acf2df382db
bimbim1214 <bimoadityapangestu@gmail.com>
2023-03-17 16:07:59 +0700

    menambahkan prosedur void input

--- bubleSort/bubleSort.cpp
+++ bubleSort/bubleSort.cpp
@@ -1,25 +1,33 @@
-// bubleSort.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 using namespace std;
 
-int a[20]   
-int a
+int a[20];
+int n;
 
+void input() {
+    while (true) {
+        cout << "masukkan banyaknya elemen pada array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai makisimal 20 elemen.\n";
+        }
+    }
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "masukan elemen array" << endl;
+    cout << "====================" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "data ke-" << (i + 1) << ": ";
+        cin >> a[i];
+    }
+}
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
